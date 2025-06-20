4c810aa2e96d9544a6833d6e32ca1f8ee8a5fc8f
AlinaYezhkova <alinayezhkova@gmail.com>
2025-06-20 19:54:47 +0300

    Pull Request corrections

--- echo/client/client.cpp
+++ echo/client/client.cpp
@@ -1,76 +1,69 @@
 #include "client.h"
 
-void Client::start()
-{
+void Client::start() {
     auto self = shared_from_this();
     fmt::println("=========Client started=========");
-    socket_.async_connect(endpoint_,
-                          [self](boost::system::error_code ec)
-                          {
-                              if (!ec)
-                              {
-                                  fmt::println("[Client] Connected to server.");
-                                  self->read_header();
-                              }
-                              else
-                              {
-                                  fmt::println(stderr, "[Client] Failed to connect: {}", ec.message());
-                              }
-                          });
+    socket_.async_connect(endpoint_, [self](boost::system::error_code ec) {
+        if (!ec) {
+            fmt::println("[Client] Connected to server.");
+            self->read_header();
+        } else {
+            fmt::println(stderr,
+                         "[Client] Failed to connect: {}",
+                         ec.message());
+        }
+    });
 }
 
-void Client::read_header()
-{
+void Client::read_header() {
     auto self = shared_from_this();
-    auto header_buf = std::make_shared<std::array<char, 4>>();
 
-    boost::asio::async_read(socket_, boost::asio::buffer(*header_buf),
-                            [self, header_buf](boost::system::error_code ec, std::size_t)
-                            {
-                                if (!ec)
-                                {
-                                    uint32_t body_length = 0;
-                                    std::memcpy(&body_length, header_buf->data(), 4);
-                                    // body_length = ntohl(body_length);
-                                    self->read_body(body_length);
-                                }
-                            });
+    auto body_length = std::make_shared<uint32_t>(0);
+    auto header_buf  = boost::asio::buffer(body_length.get(), sizeof(uint32_t));
+    boost::asio::async_read(
+        socket_,
+        header_buf,
+        [self, body_length](boost::system::error_code ec, std::size_t) {
+            if (!ec) {
+                *body_length = ntohl(*body_length);
+                self->read_body(*body_length);
+            }
+        });
 }
 
-void Client::read_body(std::size_t length)
-{
-    auto self = shared_from_this();
+void Client::read_body(uint32_t length) {
+    auto self     = shared_from_this();
     auto body_buf = std::make_shared<std::vector<char>>(length);
-    boost::asio::async_read(socket_, boost::asio::buffer(*body_buf),
-                            [self, body_buf](boost::system::error_code ec, std::size_t)
-                            {
-                                if (!ec)
-                                {
-                                    std::string msg(body_buf->begin(), body_buf->end());
-                                    fmt::println("[Message received]: {}", msg);
-                                    self->read_header();
-                                }
-                            });
+    boost::asio::async_read(
+        socket_,
+        boost::asio::buffer(*body_buf),
+        [self, body_buf](boost::system::error_code ec, std::size_t) {
+            if (!ec) {
+                std::string_view sv(body_buf->data(), body_buf->size());
+                fmt::println("[Message received]: {}", sv);
+                self->read_header();
+            }
+        });
 }
 
-void Client::run_input_thread()
-{
-    std::thread([self = shared_from_this()]
-                {
+void Client::run_input_thread() {
+    std::thread([self = shared_from_this()] {
         std::string line;
         while (std::getline(std::cin, line)) {
-            auto msg = std::make_shared<std::string>();
-            uint32_t len = static_cast<uint32_t>(line.size());
-            fmt::println("[Client sending: {} bytes]",len);
-            char header[4];
-            std::memcpy(header, &len, 4);
-            msg->assign(header, header + 4);
-            msg->append(line);
+            self->send_message(line);
+        }
+    }).detach();
+}
 
-            boost::asio::post(self->socket_.get_executor(), [self, msg]() {
-                boost::asio::async_write(self->socket_, boost::asio::buffer(*msg),
-                    [self, msg](boost::system::error_code, std::size_t) {});
-            });
-        } })
-        .detach();
+void Client::send_message(const std::string& original) {
+    auto self = shared_from_this();
+    auto msg  = std::make_shared<std::string>();
+    uint32_t len = htonl(original.size());
+    msg->append(reinterpret_cast<const char*>(&len), sizeof(len));
+    msg->append(original);
+
+    boost::asio::async_write(
+        self->socket_,
+        boost::asio::buffer(*msg),
+        [self, msg](boost::system::error_code, std::size_t) {});
 }
--- echo/server/main.cpp
+++ echo/server/main.cpp
@@ -1,18 +1,15 @@
-#include <boost/asio.hpp>
 #include <fmt/core.h>
 
-void run_server(boost::asio::io_context &io, unsigned short port);
+#include <boost/asio.hpp>
 
-int main()
-{
-    try
-    {
+void run_server(boost::asio::io_context& io, uint16_t port);
+
+int main() {
+    try {
         boost::asio::io_context io;
         run_server(io, 12345);
         io.run();
-    }
-    catch (const std::exception &e)
-    {
+    } catch (const std::exception& e) {
         fmt::println(stderr, "Server exception: {}", e.what());
     }
     return 0;
